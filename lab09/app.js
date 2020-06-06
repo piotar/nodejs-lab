a72d6ef820b24044c6cba5c86de61387faa1d016
Adam Rzońca <adam.rzonca@gmail.com>
2020-06-06 13:28:07 +0200

    Zad 3

--- lab09/app.js
+++ lab09/app.js
@@ -7,8 +7,15 @@
 // Użytkownik powinien wysłać w nagłówku token, który autoryzuje go i wpuszcza do dalszej części aplikacji.
 // Przyjmijmy że nazwa nagłówka to authorization, a wartość która wpuszcza nas do systemu to alamakota.
 
+// 3. Zmodyfikujmy zadanie 2 tak aby nagłówek authorization składał się z użytkownika
+// i hasła (wzór: login:password, np. jan:alamakota)
+// oraz sprawdżmy czy w systemie jest taki użytkownik z takim hasłem.
+// Jeżeli użytkownik istnieje, powinniśmy zmodyfikować request, aby dodać znalezionego użytkownika.
+
 const express = require("express");
 const app = express();
+
+const users = [];
 
 const logMiddleware = (req, res, next) => {
   console.log("Url:", req.url);
@@ -19,11 +26,29 @@ const logMiddleware = (req, res, next) => {
 };
 
 const authMiddleware = (req, res, next) => {
-  if (!(req.headers.authorization === "alamakota")) {
-    res.sendStatus(401);
-    return;
+  const [name, password] = req.headers.authorization.split(":");
+
+  console.log(name, password);
+
+  let user = users.find((u) => (u.name = name));
+
+  if (user) {
+    if (user.password !== password) {
+      res.sendStatus(401);
+      return;
+    }
+  } else {
+    user = { name, password };
+    users.push(user);
   }
+
   next();
+
+  //   if (!(req.headers.authorization === "alamakota")) {
+  //     res.sendStatus(401);
+  //     return;
+  //   }
+  //   next();
 };
 
 app.use(logMiddleware);
