c219752fb4901e244c2141327617d353e8e3e867
macstojak <maciej.stojak89@wp.pl>
2020-06-06 12:52:46 +0200

    second exercise

--- lab09/app.js
+++ lab09/app.js
@@ -1,17 +1,38 @@
+//1 
+// const express = require('express');
+// const app = express();
+
+// const middleware = (req, res, next) => {
+//     console.log("URL:",req.url);
+//     console.log("METHOD:",req.method);
+//     console.log("PARAMS:",req.params)
+
+//     next();
+// };
+
+// app.use(middleware);
+
+// app.get('/', (req, res) => {
+    
+// });
+
+// app.listen(4000, () => console.log('start server'));
+
+
+//2 - authorization
 const express = require('express');
 const app = express();
 
-const middleware = (req, res, next) => {
-    console.log("URL:",req.url);
-    console.log("METHOD:",req.method);
-    console.log("PARAMS:",req.params)
-
-    next();
+const checkAuth = (req, res, next) => {
+    if(req.headers.authorization){
+        console.log(req.headers.authorization)
+        next();
+    }else{
+        res.send("unauthorized user")
+    }
 };
 
-app.use(middleware);
-
-app.get('/', (req, res) => {
+app.get('/', checkAuth, (req, res) => {
     
 });
 
