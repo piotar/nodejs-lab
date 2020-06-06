fe579d3be293fd6ea883fe2d5ffe6431116955ca
RafalSzczuka <53195389+RafalSzczuka@users.noreply.github.com>
2020-06-06 13:16:34 +0200

    second task solved

--- lab09/app.js
+++ lab09/app.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const app = express();
 
-const customMiddleware = (req, res, next) => {
-  console.log(req.url);
-  console.log(req.method);
-  console.log(req.params);
+const logMiddleware = (req, res, next) => {
+  console.log(req.method, req.url);
+  console.log(req.params, req.query);
   next();
 };
 
-app.use(customMiddleware);
+const authMiddleware = (req, res, next) => {
+  if (req.headers.authorization === "alamakota") {
+    next();
+  } else {
+    res.status(401).send("unauthorized user");
+  }
+};
+
+app.use(logMiddleware);
+app.use(authMiddleware);
 
 app.get("/", (req, res) => {
   res.send("hola hola");
 });
 
-app.listen(4000, () => console.log("start server"));
+app.listen(4000);
