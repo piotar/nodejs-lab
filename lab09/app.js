bd0111d1a0d23c0a3c0dd31720b241f48b1300c0
Hanna Cylwik <hanna.cylwik@wp.pl>
2020-06-07 10:40:50 +0200

    Zadanie 3

--- lab09/app.js
+++ lab09/app.js
@@ -22,16 +22,41 @@ const app = express();
 
 //// ZADANIE 2
 
-// const url = "/:authorization";
+// const authorizationMiddleware = (req, res, next) => {
+//     req.headers.authorization === "alamakota" ? next() : res.sendStatus(401)
+// }
+
+// app.use(authorizationMiddleware);
+
+// app.get("/", (req, res) => {
+//     res.send("ok, let's go")
+// })
+
+// app.listen(3000);
+
+//// ZADANIE 3
+
+const users = [{
+    login: "ja",
+    password: "ja"
+}];
 
 const authorizationMiddleware = (req, res, next) => {
-    req.headers.authorization === "alamakota" ? next() : res.sendStatus(401)
-}
+    const [login, password] = req.headers.authorization.split(":");
+    const user = users.find(log => log.login === login)
+    if (user) {
+        console.log(user);
+        res.send(users)
+    } else {
+        users.push({ login: login, password: password })
+        res.sendStatus(401);
+    };
+};
 
 app.use(authorizationMiddleware);
 
 app.get("/", (req, res) => {
-    res.send("ok, let's go")
-})
+    res.send("ok, let's go");
+});
 
 app.listen(3000);
