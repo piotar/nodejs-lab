c485019c1b25da8e7085d97e90849e82011c7fc3
Agnieszka Jarosik <jarosikloj@gmail.com>
2020-06-07 09:43:28 +0200

    2

--- lab09/app.js
+++ lab09/app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
 
+// app.use((req, res, next)=>{
+//   console.log(req.url);
+//   console.log(req.params);
+//   console.log(req.query);
+//   console.log(req.method);
+//   next();
+// })
+
 app.use((req, res, next)=>{
-  console.log(req.url);
-  console.log(req.params);
-  console.log(req.query);
-  console.log(req.method);
-  next();
+  const password = req.headers.authorization;
+  if(password==='alamakota'){
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+  
 })
 
 app.get('/', (req, res)=>{
@@ -18,7 +28,7 @@ app.get('/:id', (req, res)=>{
   res.send(req.params);
 })
 
-app.post('', (req, res)=>{
+app.post('/', (req, res)=>{
   res.sendStatus(200);
 })
 
