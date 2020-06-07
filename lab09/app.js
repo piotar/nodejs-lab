bfc612c3496adcdb519f5e35ac449e8fd1ce2324
Katarzyna Górska <katarzyna.redlinska@gmail.com>
2020-06-07 11:03:40 +0200

    zadanie 4

--- lab09/app.js
+++ lab09/app.js
@@ -51,13 +51,63 @@
 // np. jan:alamakota) oraz sprawdżmy czy w systemie jest taki użytkownik z takim hasłem. Jeżeli użytkownik istnieje,
 // powinniśmy zmodyfikować request, aby dodać znalezionego użytkownika.
 
+// const express = require("express");
+// const app = express();
+
+// const users = [
+//   { login: "jan", password: "alamakota" },
+//   { login: "kasia", password: "gorska" },
+// ];
+
+// const customMiddleware = (req, res, next) => {
+//   console.log(`Address: ${req.url}`, `Method: ${req.method}`);
+//   console.log(req.query, req.params);
+//   next();
+// };
+
+// const authorizationMiddleware = (req, res, next) => {
+//   const [login, password] = req.headers.authorization.split(":");
+// };
+
+// console.log(login, password);
+
+// let user = users.find((u) => u.login === login);
+
+// if (user && user.password === password) {
+//   req.user = user;
+//   next();
+// } else {
+//   res.sendStatus(401);
+// }
+
+// app.use(customMiddleware);
+// app.use(authorizationMiddleware);
+
+// app.get("/", (req, res) => {
+//   res.sendStatus(200).send(req.user);
+// });
+
+// app.listen(4000, () => console.log("start server"));
+
+// Zadanie 4
+// Wykorzystując zewnętrzny middleware body-parser, odczytajmy zawartość żądania typu text i sprawdźmy czy w przesłanym
+// przez użytkownika tekście nie zostały umieszczone niecenzuralne słowa. Jeżeli jakieś słowo podane ze słownika znajduje
+// się w żądaniu zakończmy cykl wysyłając błąd dla użytkownika końcowego(status błędu 400). Przykładowy słowinik zakazanych
+// słów: ['disco polo', 'piwo', 'hazard', 'cukierki']
+
+// Stwórzmy tutaj dodatkowo REST API do zapisu i wyświetlania zawartości przesłanego tekstu przez użytkownika. Zapiszmy
+// to na dysku w pliku tektowym
+
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 const users = [
   { login: "jan", password: "alamakota" },
   { login: "kasia", password: "gorska" },
 ];
+
+const dictionary = ["disco polo", "piwo", "hazard", "cukierki"];
 
 const customMiddleware = (req, res, next) => {
   console.log(`Address: ${req.url}`, `Method: ${req.method}`);
@@ -80,23 +130,23 @@ if (user && user.password === password) {
   res.sendStatus(401);
 }
 
-app.use(customMiddleware);
-app.use(authorizationMiddleware);
+// app.use(customMiddleware);
+// app.use(authorizationMiddleware);
+app.use(bodyParser.text());
 
 app.get("/", (req, res) => {
-  res.sendStatus(200);
+  res.sendStatus(200).send(req.user);
+});
+
+app.post("/", (req, res) => {
+  if (dictionary.some((word) => req.body.includes(word))) {
+    res.status(400).send("Zakazane słowo");
+  } else {
+    res.send("Tekst jest poprawny");
+  }
 });
 
 app.listen(4000, () => console.log("start server"));
-
-// Zadanie 4
-// Wykorzystując zewnętrzny middleware body-parser, odczytajmy zawartość żądania typu text i sprawdźmy czy w przesłanym
-// przez użytkownika tekście nie zostały umieszczone niecenzuralne słowa. Jeżeli jakieś słowo podane ze słownika znajduje
-// się w żądaniu zakończmy cykl wysyłając błąd dla użytkownika końcowego(status błędu 400). Przykładowy słowinik zakazanych
-// słów: ['disco polo', 'piwo', 'hazard', 'cukierki']
-
-// Stwórzmy tutaj dodatkowo REST API do zapisu i wyświetlania zawartości przesłanego tekstu przez użytkownika. Zapiszmy
-// to na dysku w pliku tektowym
 
 // Zadanie 5
 // Stwórzmy middleware, który sprawdzi czy podana ścieżka jest ścieżką do pliku fizycznego na dysku. Jeżeli tak to powinniśmy
