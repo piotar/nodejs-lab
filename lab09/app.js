61e3360a94577887a09ee42d81ffd04272f59adf
Bern <bernard.kulegowski@gmail.com>
2020-06-07 13:58:57 +0200

    ZAD 2

--- lab09/app.js
+++ lab09/app.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const app = express();
+//1
 
-const customMiddleware = (req, res, next) => {
-    // some logic ...
-    next();
-};
+// const customMiddleware = (req, res, next) => {
+//     // console.log(`Address: ${req.url}, Method:${req.method}`)
+//     // console.log(req.query, req.params)                 // some logic ...
+//     // next();
+//     console.log("Request URL:", req.originalUrl);
+//     console.log("Request Type", req.method);
+//     console.log("Log Params", req.params);
+//     next();
+// };
 
-app.use(customMiddleware);
 
-app.get('/', (req, res) => {
-    // ...
-});
+// app.use(customMiddleware);
+
+// app.get('/', (req, res) => {
+//     res.send("hello!");
+// });
+//2
+
+app.use((req, res, next) => {
+    const password = req.headers.authorization;
+    if (password != 'alamakota') {
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+
+})
+
+
+
 
 app.listen(4000, () => console.log('start server'));
