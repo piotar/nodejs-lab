3abfdf0d94d5174e7cb7df22a7fc3989e366174f
lulek92 <lulmichal@gmail.com>
2020-06-07 18:05:03 +0200

    final commit

--- lab09/app.js
+++ lab09/app.js
@@ -2,28 +2,30 @@ const express = require("express");
 
 const app = express();
 
-const logStuff = (req, res, next) => {
-  console.log("Request URL:", req.originalUrl);
-  console.log("Request Type:", req.method);
-  console.log("Log Params:", req.params);
-  next();
-};
+// const logOriginalUrl = (req, res, next) => {
+//   console.log("Request URL:", req.originalUrl);
+//   next();
+// };
 
-app.use(logStuff);
+// const logMethod = (req, res, next) => {
+//   console.log("Request Type:", req.method);
+//   next();
+// };
 
-app.get("/:name?", logStuff, function (req, res) {
-  res.send("halo!");
+// const logParams = (req, res, next) => {
+//   console.log("Log Params:", req.params);
+//   next();
+// };
+
+// let logStuff = [logOriginalUrl, logMethod, logParams, logHeaders, logBody];
+
+const chatRouter = require("./chat");
+
+app.use("/", chatRouter);
+
+app.use((error, req, res, next) => {
+  res.sendStatus(400);
+  console.log(error);
 });
-
-// const chatRouter = require("./chat");
-
-// app.use("/", chatRouter, function (req, res) {
-//   res.sendStatus(401);
-// });
-
-// app.use((error, req, res, next) => {
-//   res.sendStatus(400);
-//   console.log(error);
-// });
 
 app.listen(4000, () => console.log("start server"));
