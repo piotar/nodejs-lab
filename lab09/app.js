f00e6b004bb331eb0e1bac677f698cc481139c76
Daniel Majewski <danielmajewski82@gmail.com>
2020-06-07 13:25:20 +0200

    first task

---
+++ lab09/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+const myMiddleware = (req, res, next) => {
+  console.log("request URL:", req.originalUrl);
+  console.log("request type:", req.method);
+  console.log("log params:", req.params);
+  next();
+};
+
+app.use(myMiddleware);
+
+app.get("/:name?", myMiddleware, (req, res) => {
+  res.send("halo");
+});
+
+app.listen(4000, () => console.log("start server"));
