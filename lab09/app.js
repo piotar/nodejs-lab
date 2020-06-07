4fff71d0bd5062d330fa423fd0c13e9a02bbd685
Katarzyna Górska <katarzyna.redlinska@gmail.com>
2020-06-07 10:38:58 +0200

    zadanie 3

--- lab09/app.js
+++ lab09/app.js
@@ -25,31 +25,69 @@
 // Użytkownik powinien wysłać w nagłówku token, który autoryzuje go i wpuszcza do dalszej części aplikacji.
 // Przyjmijmy że nazwa nagłówka to authorization, a wartość która wpuszcza nas do systemu to alamakota.
 
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-const checkAuthorization = (req, res, next) => {
-  //   console.log(`Address: ${req.url}`, `Method: ${req.method}`);
-  //   console.log(req.query, req.params);
-  if (req.headers.authorization === "alamakota") {
-    next();
-  } else {
-    res.status(401).send("Unauthorized user");
-  }
-};
+// const checkAuthorization = (req, res, next) => {
+//   //   console.log(`Address: ${req.url}`, `Method: ${req.method}`);
+//   //   console.log(req.query, req.params);
+//   if (req.headers.authorization === "alamakota") {
+//     next();
+//   } else {
+//     res.status(401).send("Unauthorized user");
+//   }
+// };
 
-app.use(checkAuthorization);
+// app.use(checkAuthorization);
 
-app.get("/", (req, res) => {
-  res.send("Authorized user");
-});
+// app.get("/", (req, res) => {
+//   res.send("Authorized user");
+// });
 
-app.listen(4000, () => console.log("start server"));
+// app.listen(4000, () => console.log("start server"));
 
 // Zadanie 3
 // Zmodyfikujmy zadanie 2 tak aby nagłówek authorization składał się z użytkownika i hasła (wzór: login:password,
 // np. jan:alamakota) oraz sprawdżmy czy w systemie jest taki użytkownik z takim hasłem. Jeżeli użytkownik istnieje,
 // powinniśmy zmodyfikować request, aby dodać znalezionego użytkownika.
+
+const express = require("express");
+const app = express();
+
+const users = [
+  { login: "jan", password: "alamakota" },
+  { login: "kasia", password: "gorska" },
+];
+
+const customMiddleware = (req, res, next) => {
+  console.log(`Address: ${req.url}`, `Method: ${req.method}`);
+  console.log(req.query, req.params);
+  next();
+};
+
+const authorizationMiddleware = (req, res, next) => {
+  const [login, password] = req.headers.authorization.split(":");
+};
+
+console.log(login, password);
+
+let user = users.find((u) => u.login === login);
+
+if (user && user.password === password) {
+  req.user = user;
+  next();
+} else {
+  res.sendStatus(401);
+}
+
+app.use(customMiddleware);
+app.use(authorizationMiddleware);
+
+app.get("/", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.listen(4000, () => console.log("start server"));
 
 // Zadanie 4
 // Wykorzystując zewnętrzny middleware body-parser, odczytajmy zawartość żądania typu text i sprawdźmy czy w przesłanym
@@ -61,3 +99,6 @@ app.listen(4000, () => console.log("start server"));
 // to na dysku w pliku tektowym
 
 // Zadanie 5
+// Stwórzmy middleware, który sprawdzi czy podana ścieżka jest ścieżką do pliku fizycznego na dysku. Jeżeli tak to powinniśmy
+// ten plik wysłać do klienta. Jeżeli jednak nie możemy odnaleźć, powinniśmy przesłać dalej wykonywanie naszego żądania(tutaj
+// może być pomocny moduł fs).
