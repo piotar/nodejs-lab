e2f4da97e82ab74ae0e53bcb1049b409bdfe7205
Hanna Cylwik <hanna.cylwik@wp.pl>
2020-06-07 09:54:44 +0200

    Zadanie 2

--- lab09/app.js
+++ lab09/app.js
@@ -3,19 +3,35 @@ const app = express();
 
 //// ZADANIE 1
 
-const customMiddleware = (req, res, next) => {
-    console.log(
-        `Address: ${req.url}, Method: ${req.method}`
-    );
-    console.log(req.query, req.params);
+// const customMiddleware = (req, res, next) => {
+//     console.log(
+//         `Address: ${req.url}, Method: ${req.method}`
+//     );
+//     console.log(req.query, req.params);
 
-    next();
-};
+//     next();
+// };
 
-app.use(customMiddleware);
+// app.use(customMiddleware);
 
-app.get("/:id?", (req, res) => {
-    res.send("ok");
-});
+// app.get("/:id?", (req, res) => {
+//     res.send("ok");
+// });
 
-app.listen(3000, () => console.log("start server"));
+// app.listen(3000, () => console.log("start server"));
+
+//// ZADANIE 2
+
+// const url = "/:authorization";
+
+const authorizationMiddleware = (req, res, next) => {
+    req.headers.authorization === "alamakota" ? next() : res.sendStatus(401)
+}
+
+app.use(authorizationMiddleware);
+
+app.get("/", (req, res) => {
+    res.send("ok, let's go")
+})
+
+app.listen(3000);
