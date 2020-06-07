77db523a0f087110ae7f73fbee00f1990af1756a
Piotr Dyrda <piotr.dyrda88@gmail.com>
2020-06-07 10:16:06 +0200

    modified authorization

--- lab09/app.js
+++ lab09/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+const users = [
+  {
+    id: 0,
+    name: 'Piotr',
+    login: {
+      userName: 'piotr',
+      password: 'alamakota',
+    },
+  },
+];
+
 // const showInfo = (req, res, next) => {
 //   console.log('Params: ', req.params);
 //   console.log('Method: ', req.method);
@@ -12,17 +23,21 @@ const app = express();
 // app.use(showInfo);
 
 const authorize = (req, res, next) => {
-  if (req.headers.authorization === 'alamakota') {
+  const [login, password] = req.headers.authorization.split(':');
+  const user = users.find((u) => u.login.userName === login && u.login.password === password);
+
+  if (user) {
+    req.user = { id: user.id, name: user.name };
     next();
   } else {
-    res.sendStatus(403);
+    res.sendStatus(401);
   }
 };
 
 app.use(authorize);
 
 app.get('/', (req, res) => {
-  res.send('Hello it works');
+  res.status(200).send(req.user);
 });
 
 app.listen(4700, () => {
