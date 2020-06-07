c1664aae5913fde328d9e8a72a71150a9056a7fc
Daniel Iwaszko <pepiozo@mail.com>
2020-06-07 10:49:12 +0200

    added changes to 3 exercise

--- lab09/app.js
+++ lab09/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+const users = [
+    {
+        login: 'Daniel',
+        password: 'mojehaslo'
+    },
+    {
+        login: 'Olek',
+        password: 'innehaslo'
+    }
+]
+
 const customMiddleware = (req, res, next) => {
     console.log(`Address: ${req.url}, Method: ${req.method}`);
     console.log(req.query, req.params);
@@ -9,15 +20,32 @@ const customMiddleware = (req, res, next) => {
 };
 
 // zad 2. Autoryzacja dostepu do tresci w postman w headerze key = Authorization oraz value = alamakota
-app.use((req, res, next)=>{
-    const password = req.headers.authorization;
-    if(password === 'alamakota'){
+// app.use((req, res, next) => {
+//     const password = req.headers.authorization;
+//     if(password === 'alamakota'){
+//       next();
+//     } else {
+//       res.sendStatus(401);
+//     }
+// })
+
+// zad 3.
+const authMiddleware = (req, res, next) => {
+    const [login, password] = req.headers.authorization.split(":");
+  
+    console.log(login, password);
+  
+    let user = users.find((u) => u.login === login);
+  
+    if (user && user.password === password) {
+      req.user = user;
       next();
     } else {
       res.sendStatus(401);
     }
-})
+  };
 
+app.use(authMiddleware);
 app.use(customMiddleware);
 
 app.get('/:id?', /* customMiddleware ,*/ (req, res) => {
