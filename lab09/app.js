875cd1cda7b4623b91f90e66b1ec43dbfef08150
dante74 <dan_te@wp.pl>
2020-06-07 09:49:14 +0200

    first

--- lab09/app.js
+++ lab09/app.js
@@ -2,25 +2,47 @@ var express = require("express");
 var app = express();
 
 //Zadanie 1
-const logger = (req, res, next) => {
-    console.log(req.method);
-    console.log(req.url);
-    console.log(req.path);
+// const logger = (req, res, next) => {
+//     console.log(req.method);
+//     console.log(req.url);
+//     console.log(req.path);
 
-    console.dir(req.query);
-    const { id } = req.query;
-    id ? console.log(id) : console.log("Brak parametrów");
+//     console.dir(req.query);
+//     const { id } = req.query;
+//     id ? console.log(id) : console.log("Brak parametrów");
 
 
-    next();
-}
-const parameter = (req, res) => {
+//     next();
+// }
+// const parameter = (req, res) => {
 
-    res.send("Koniec");
-}
-app.use(logger);
+//     res.send("Koniec");
+// }
+// app.use(logger);
 //Zad1
 //Zad2
+app.use((req, res, next) => {
+    const password = req.headers.authorization;
+    if (password === 'alamakota') {
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+
+})
+
+app.get('/', (req, res) => {
+    res.sendStatus(200);
+})
+
+app.get('/:id', (req, res) => {
+
+    res.send(req.params);
+})
+
+app.post('/', (req, res) => {
+    res.sendStatus(200);
+})
 
 //Zad2
 
