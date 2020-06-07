f684c56bedd0dd4129a364529e7eeef67c34f1f3
Bern <bernard.kulegowski@gmail.com>
2020-06-07 14:38:04 +0200

    ZAD 3

--- lab09/app.js
+++ lab09/app.js
@@ -18,19 +18,56 @@ const app = express();
 // app.get('/', (req, res) => {
 //     res.send("hello!");
 // });
+
+
 //2
 
-app.use((req, res, next) => {
-    const password = req.headers.authorization;
-    if (password != 'alamakota') {
+// app.use((req, res, next) => {
+//     const password = req.headers.authorization;
+//     if (password != 'alamakota') {
+//         next();
+//     } else {
+//         res.sendStatus(401);
+//     }
+
+// })
+
+//3
+
+const users = [{
+    login: "admin",
+    password: "1234",
+    name: "jas",
+    lastName: "nowak"
+},
+{
+    login: "admin2",
+    password: "01234",
+    name: "Adam",
+    lastName: "Mowak"
+}];
+
+const authMiddleware = (req, res, next) => {
+    const [login, password] = req.headers.authorization || "".split(":");
+
+    const user = users.find(log => log.login === login && log.password === password)
+    if (user) {
+        req.user = user;
         next();
     } else {
+        // users.push({ login: login, password: password })
         res.sendStatus(401);
-    }
+    };
+};
 
-})
+app.use(authMiddleware);
 
+app.get("/", (req, res) => {
+    res.send(`hello ${req.user.name}!`);
+});
 
 
 
 app.listen(4000, () => console.log('start server'));
+
+//4
