1ad7f70214adcfbec430bd1898d696a739d77460
Piotr Dyrda <piotr.dyrda88@gmail.com>
2020-06-07 10:35:30 +0200

    fix authorization

--- lab09/app.js
+++ lab09/app.js
@@ -1,14 +1,14 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+
 const app = express();
 
 const users = [
   {
     id: 0,
     name: 'Piotr',
-    login: {
-      userName: 'piotr',
-      password: 'alamakota',
-    },
+    userName: 'piotr',
+    password: 'alamakota',
   },
 ];
 
@@ -24,10 +24,10 @@ const users = [
 
 const authorize = (req, res, next) => {
   const [login, password] = req.headers.authorization.split(':');
-  const user = users.find((u) => u.login.userName === login && u.login.password === password);
+  const user = users.find((u) => u.userName === login && u.password === password);
 
   if (user) {
-    req.user = { id: user.id, name: user.name };
+    req.user = user;
     next();
   } else {
     res.sendStatus(401);
