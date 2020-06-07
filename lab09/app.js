adbe4a67b2bf438e6d7db2b24510f12743efdaa7
Hanna Cylwik <hanna.cylwik@wp.pl>
2020-06-07 11:09:17 +0200

    Zadanie 4

--- lab09/app.js
+++ lab09/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
 
 //// ZADANIE 1
 
@@ -36,27 +37,48 @@ const app = express();
 
 //// ZADANIE 3
 
-const users = [{
-    login: "ja",
-    password: "ja"
-}];
+// const users = [{
+//     login: "ja",
+//     password: "ja"
+// }];
 
-const authorizationMiddleware = (req, res, next) => {
-    const [login, password] = req.headers.authorization.split(":");
-    const user = users.find(log => log.login === login)
-    if (user) {
-        console.log(user);
-        res.send(users)
-    } else {
-        users.push({ login: login, password: password })
-        res.sendStatus(401);
-    };
-};
+// const authorizationMiddleware = (req, res, next) => {
+//     const [login, password] = req.headers.authorization.split(":");
+//     const user = users.find(log => log.login === login)
+//     if (user) {
+//         console.log(user);
+//         res.send(users)
+//     } else {
+//         users.push({ login: login, password: password })
+//         res.sendStatus(401);
+//     };
+// };
 
-app.use(authorizationMiddleware);
+// app.use(authorizationMiddleware);
+
+// app.get("/", (req, res) => {
+//     res.send("ok, let's go");
+// });
+
+// app.listen(3000);
+
+//// ZADANIE 4
+
+const forbiddenWords = ['disco polo', 'piwo', 'hazard', 'cukierki'];
+
+app.use(bodyParser.text());
 
 app.get("/", (req, res) => {
-    res.send("ok, let's go");
+    const ifForbid = forbiddenWords.some(w => req.body.includes(w))
+    if (ifForbid) {
+        res.status(400).send("Forbidden word");
+    } else {
+        res.send("ok, let's go");
+    };
 });
+
+// app.get("/", (req, res) => {
+//     res.send("ok, let's go");
+// });
 
 app.listen(3000);
