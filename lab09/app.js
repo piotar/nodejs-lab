05f99de53c0b69cbb02bae5744a9db233076e361
Katarzyna Górska <katarzyna.redlinska@gmail.com>
2020-06-07 09:53:03 +0200

    zadanie 2

--- lab09/app.js
+++ lab09/app.js
@@ -3,24 +3,45 @@
 // żądań do serwera i wyświetlenie w konsoli informacji na jaki adres użytkownik próbuje się dostać, jaką metodą oraz
 // czy zawiera w sobie parametry.
 
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-const customMiddleware = (req, res, next) => {
-  console.log(`Address: ${req.url}`, `Method: ${req.method}`);
-  console.log(req.query, req.params);
-  next();
-};
+// const customMiddleware = (req, res, next) => {
+//   console.log(`Address: ${req.url}`, `Method: ${req.method}`);
+//   console.log(req.query, req.params);
+//   next();
+// };
 
-app.use(customMiddleware);
+// app.use(customMiddleware);
 
-app.get("/:id?", (req, res) => {
-  res.send("OK");
-});
+// app.get("/:id?", (req, res) => {
+//   res.send("OK");
+// });
 
-app.listen(4000, () => console.log("start server"));
+// app.listen(4000, () => console.log("start server"));
 
 // Zadanie 2
 // Kolejnym zadaniem jest stworzenie middleware, który zabezpieczy naszą aplikację dla osób nie upoważnionych.
 // Użytkownik powinien wysłać w nagłówku token, który autoryzuje go i wpuszcza do dalszej części aplikacji.
 // Przyjmijmy że nazwa nagłówka to authorization, a wartość która wpuszcza nas do systemu to alamakota.
+
+const express = require("express");
+const app = express();
+
+const checkAuthorization = (req, res, next) => {
+  //   console.log(`Address: ${req.url}`, `Method: ${req.method}`);
+  //   console.log(req.query, req.params);
+  if (req.headers.authorization === "alamakota") {
+    next();
+  } else {
+    res.status(401).send("unauthorized user");
+  }
+};
+
+app.use(checkAuthorization);
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(4000, () => console.log("start server"));
