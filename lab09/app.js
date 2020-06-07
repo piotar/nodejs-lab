7bfd991113cd799309475c579e9a257e23b3ff82
Karol Chilimoniuk <webdevkarolchilimoniuk@gmail.com>
2020-06-07 09:58:44 +0200

    zadanie 2

--- lab09/app.js
+++ lab09/app.js
@@ -6,17 +6,7 @@ const app = express();
 
 // zad 1
 
-const customMiddleware = (req, res, next) => {
-    const reqUrl = req.url;
-    const reqMethod = req.method;
-    // const reqParams = req.query;
-    const reqParams = req.params;
-    console.log(`show adress: ${reqUrl}, 
-                 method: ${reqMethod},
-                 params${reqParams ? reqParams : "nothing"}`);
-    next();
-}
-// app.use((req, res, next) => {
+// const customMiddleware = (req, res, next) => {
 //     const reqUrl = req.url;
 //     const reqMethod = req.method;
 //     // const reqParams = req.query;
@@ -25,14 +15,31 @@ const customMiddleware = (req, res, next) => {
 //                  method: ${reqMethod},
 //                  params${reqParams ? reqParams : "nothing"}`);
 //     next();
-// })
+// }
+//app.use(customMiddleware);
+// app.get('/:id?',(req, res) => {
+//     res.send('Pozdrawiam');
+// });
 
-app.use(customMiddleware);
 
-app.get('/:id?',(req, res) => {
+// zad 2
+
+
+const authorizationMiddleware = (req, res, next) => {
+    if(req.headers.authorization === "alamakota") {
+       res.send('Dostęp potwierdzony');
+    } else {
+        res.sendStatus(401);
+    }
+}
+app.use(authorizationMiddleware);
+
+
+
+
+app.get('/',(req, res) => {
     res.send('Pozdrawiam');
 });
-
 app.listen(5000, ()=> {
     console.log(`it works`);
 })
