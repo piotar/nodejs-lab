eb9bc8502651347456462b5d93e559509896a7d9
Piotr Dyrda <piotr.dyrda88@gmail.com>
2020-06-18 16:59:13 +0200

    add fs module

--- lab09/app.js
+++ lab09/app.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
@@ -14,18 +15,19 @@ const users = [
 
 const forbiddenWords = ['disco polo', 'piwo', 'hazard', 'cukierki'];
 
-// const showInfo = (req, res, next) => {
-//   console.log('Params: ', req.params);
-//   console.log('Method: ', req.method);
-//   console.log('Host: ', req.headers.host);
+const showInfo = (req, res, next) => {
+  console.log('Params: ', req.params);
+  console.log('Method: ', req.method);
+  console.log('Host: ', req.headers.host);
+  console.log('Query: ', req.query);
 
-//   next();
-// };
-
-// app.use(showInfo);
+  next();
+};
 
 const authorize = (req, res, next) => {
-  const [login, password] = req.headers.authorization.split(':');
+  const { authorization = '' } = req.headers;
+  const [login, password] = authorization.split(':');
+
   const user = users.find((u) => u.userName === login && u.password === password);
 
   if (user) {
@@ -37,26 +39,38 @@ const authorize = (req, res, next) => {
 };
 
 app.use(bodyParser.text());
+app.use(showInfo);
 
-app.get('/', async (req, res) => {
+app.get('/file', (req, res) => {
+  fs.readFile('data.txt', 'utf-8', (err, data) => {
+    if (data) {
+      res.status(200).send(data);
+    } else {
+      res.status(404).send(err);
+    }
+  });
+});
+
+app.post('/file', async (req, res) => {
   try {
     const text = req.body.split();
+
     const isForbidden = forbiddenWords.some((w) => req.body.includes(w));
 
     if (isForbidden) {
       res.status(400).send('Forbidden word');
     } else {
-      res.send('Ok');
+      fs.writeFile('data.txt', req.body, () => {
+        res.status(200).send('Saved');
+      });
     }
   } catch (err) {
     console.log(err.message);
   }
 });
 
-app.use(authorize);
-
-app.get('/user', (req, res) => {
-  res.status(200).send(req.user);
+app.get('/:name', authorize, (req, res) => {
+  res.send(`Hello ${req.params.name}`);
 });
 
 app.listen(4700, () => {
