adeae2bcbef4ea664efa71865e5b3b3135ba2b33
MaciejKasperowicz <56731981+MaciejKasperowicz@users.noreply.github.com>
2020-06-21 13:11:03 +0200

    1 program

--- lab10/app1.js
+++ lab10/app1.js
@@ -1,4 +1,18 @@
 const express = require("express");
 const app = express();
 
+console.log("pug");
 //test
+app.set("view engine", "pug");
+
+
+app.get("/:name?", (req, res) => {
+    const { name = "world" } = req.params;
+
+    res.render("index", {
+        title: "hello",
+        message: `Hello ${name}!`
+    })
+})
+
+app.listen(4000, () => console.log("start server"));
