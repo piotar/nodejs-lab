c914cfd7a37642f3a8aedef26bf8dbb5fe4eb683
dante74 <dan_te@wp.pl>
2020-06-21 09:44:42 +0200

    lab2

--- lab10/app.js
+++ lab10/app.js
@@ -1,26 +1,41 @@
-//var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-
+var mustacheExpress = require('mustache-express');
 
 var app = express();
 
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-app.get('/', (req, res, next) => {
-    res.render('index', { title: 'Hello', name: 'World' });
-});
+// app.get('/', (req, res, next) => {
+//     res.render('index', { title: 'Hello', name: 'World' });
+// });
 
-app.get('/:name', (req, res, next) => {
-    let { name } = req.params;
-    res.render('index', { title: 'Hello', name: `${name}` });
-});
+// app.get('/:name', (req, res, next) => {
+//     let { name } = req.params;
+//     res.render('index', { title: 'Hello', name: `${name}` });
+// });
 
+
+app.get('/:price/:tax', (req, res, next) => {
+    const { price, tax } = req.params;
+    let taxResult = tax * price / 100;
+    let taxFree = price - taxResult;
+    console.log(taxResult);
+    console.log(taxFree);
+    let data = {
+        price,
+        tax,
+        taxResult,
+        taxFree,
+    };
+    res.render('index', data);
+});
 
 app.listen(3000, () => console.log("Serwer działaaaaaa"));
