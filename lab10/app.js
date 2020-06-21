3800bbde6d78edf32af2de16f7127349546f51ba
Agnieszka Jarosik <jarosikloj@gmail.com>
2020-06-21 11:03:24 +0200

    zadanie 5

--- lab10/app.js
+++ lab10/app.js
@@ -12,25 +12,34 @@ app.set('views', __dirname + '/views');
 
 // app.set('view engine', 'pug');
 
-const calculateTax = (tax, amount) => {
-  const taxValue = tax * amount / 100;
-  return {
-    taxValue: taxValue,
-    amount: amount - taxValue
-  };
-}
+// const calculateTax = (tax, amount) => {
+//   const taxValue = tax * amount / 100;
+//   return {
+//     taxValue: taxValue,
+//     amount: amount - taxValue
+//   };
+// }
 
-app.get('/:name?', function (req, res) {
-  const name = req.params.name || 'World';
-  const scope = { title: 'some title', header: `Heloo ${name}!` };
-  res.render('index', scope);
-});
+// app.get('/:name?', function (req, res) {
+//   const name = req.params.name || 'World';
+//   const scope = { title: 'some title', header: `Heloo ${name}!` };
+//   res.render('index', scope);
+// });
 
-app.get('/podatek/:tax/:price', (req, res, next) => {
-  const { tax, price } = req.params;
-  const result = calculateTax(tax, price);
-  res.render('index', {price: price, tax: tax, ...result});
-});
+// app.get('/podatek/:tax/:price', (req, res, next) => {
+//   const { tax, price } = req.params;
+//   const result = calculateTax(tax, price);
+//   res.render('index', {price: price, tax: tax, ...result});
+// });
+
+// app.get('/:a/:b', (req, res, next) => {
+//     const {a,b} = req.params;
+//     if( Number(b) === 0 ) {
+//       throw new Error('dzielenie przez 0!');
+//     } else {
+//       res.send( (a/b).toString() );
+//     }
+// });
 
 const readFileAsync = util.promisify(fs.readFile);
 
@@ -38,28 +47,19 @@ app.get('/file/:path', async(req, res, next) => {
   try {
     const { path } = req.params;
     const data = await(readFileAsync(`./static/${path}`, 'utf-8'));
-    if( data ) {
-      res.send(data);
-    } else {
-      throw new Error('no such file or directory');
-    }
+    data && res.send(data);
   } catch (e) {
     next(e)
   }
 });
 
-app.get('/:a/:b', (req, res, next) => {
-    const {a,b} = req.params;
-    if( Number(b) === 0 ) {
-      throw new Error('dzielenie przez 0!');
-    } else {
-      res.send( (a/b).toString() );
-    }
-});
-
 app.use((e, req, res, next) => {
-  console.log(e.message);
-  res.status(500).send(e.message);
+  if(e.code === 'ENOENT') {
+    res.render('error');
+  } else {
+    console.log(e);
+    res.status(500).send(e.message);
+  }
 });
 
 app.listen(4500, () => console.log('start server'));
