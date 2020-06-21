790d5735645c88943208bd51f1116c2ded527bd7
lulek92 <lulmichal@gmail.com>
2020-06-21 14:57:22 +0200

    except last task

--- lab10/firstTask.js
+++ lab10/firstTask.js
@@ -2,10 +2,6 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "pug");
-// app.get("/:name", function (req, res) {
-//   const scope = { title: "some title", header: "heloo!" };
-//   res.render("index", scope);
-// });
 
 app.get("/:name?", function (req, res) {
   const { name = "World" } = req.params;
