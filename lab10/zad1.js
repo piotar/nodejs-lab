03cee72bcd83c72168f17d28f59027f2909d1608
Krzysiek <krzysztof.marciniak.123@gmail.com>
2020-06-21 13:13:40 +0200

    zad1 zmiana

--- lab10/zad1.js
+++ lab10/zad1.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const app = express();
 
-
 app.set("view engine", "pug");
 
-
-app.get('/:name?', function (req, res) {
-    const {name= "World"} = { req.params };
-    res.render("index", {
-        title: "Hello",
-        message: `Hello ${name}!`,
+app.get("/:name?", function (req, res) {
+  const { name = "World" } = req.params;
+  res.render("index", {
+    title: "Hello",
+    message: `Hello ${name}!`,
   });
 });
 
