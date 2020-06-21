62f90e44d9fdec1a836de1cf283b226c11e45281
Skoboanna <anna@skobo.pl>
2020-06-21 17:19:37 +0200

    2

--- lab10/2-tax.js
+++ lab10/2-tax.js
@@ -1,21 +1,26 @@
-// 2 - Przećwiczmy wczytywanie szablonów zmieniając system szablonów PUG na MUSTACHE(https://github.com/bryanburgers/node-mustache-express). 
+// 2 - Przećwiczmy wczytywanie szablonów zmieniając system szablonów PUG na
+// MUSTACHE(https://github.com/bryanburgers/node-mustache-express).
 // Stwórzmy aplikację, która wyliczy nam podatek z podanej kwoty i zwróci nam widok z danymi.
 // Przykład ścieżki /podatek/19/25, gdzie 19 to wysokość vat podana w procentach oraz 25 kwota.
 
-const express = require('express');
+const express = require("express");
+const mustacheExpress = require("mustache-express");
 const app = express();
 
-app.set('view engine', 'pug')
-    app.get('/', function (req, res) {
-        
-        const message = "hello"
-        res.render('index', { title: 'Hey', message: message })
-    });
 
-    app.get('/:name', function (req, res) {
-        let name = req.params.name? req.params.name : "";
-        const message = "hello " + name;
-        res.render('index', { title: 'Hey', message: message })
-    });
+const calc = (a, b) => {
+    return a *(b * 0.1);
+}
+// app.set('view engine', 'pug')
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
 
-app.listen(4000, () => console.log('start server'));
+app.get("/podatek/:tax/:price", function (req, res) {
+  const {price, amount} = req.params;
+
+  const result = calc(price, amount);
+
+  res.render("index", {price, amount, result });
+});
+
+app.listen(4000, () => console.log("start server"));
---
+++ lab10/render.js
@@ -0,0 +1,6 @@
+
+    function renderHello() {
+        var template = document.getElementById('template').innerHTML;
+        var rendered = Mustache.render(template, { name: 'Luke' });
+        document.getElementById('target').innerHTML = rendered;
+      }
