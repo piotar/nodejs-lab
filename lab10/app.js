ff19da6d229e4b6f1919654e0697fbf043166bff
dante74 <dan_te@wp.pl>
2020-06-21 10:27:35 +0200

    lab3

--- lab10/app.js
+++ lab10/app.js
@@ -22,17 +22,34 @@ app.use(express.urlencoded({ extended: false }));
 // });
 
 
-app.get('/:price/:tax', (req, res, next) => {
-    const { price, tax } = req.params;
-    let taxResult = tax * price / 100;
-    let taxFree = price - taxResult;
-    let data = {
-        price,
-        tax,
-        taxResult,
-        taxFree,
-    };
-    res.render('index', data);
-});
+// app.get('/:price/:tax', (req, res, next) => {
+//     const { price, tax } = req.params;
+//     let taxResult = tax * price / 100;
+//     let taxFree = price - taxResult;
+//     let data = {
+//         price,
+//         tax,
+//         taxResult,
+//         taxFree,
+//     };
+//     res.render('index', data);
+// });
 
+const div = (a, b) => {
+    if (b === 0) {
+        throw new Error('divide by 0');
+    }
+    return a / b;
+}
+
+app.get('/:var1/:var2', (req, res, next) => {
+    const { var1, var2 } = req.params;
+
+    try {
+        const result = div(var1, var2);
+        console.log(result);
+    } catch (error) {
+        console.log(error.message);
+    }
+});
 app.listen(3000, () => console.log("Serwer działaaaaaa"));
