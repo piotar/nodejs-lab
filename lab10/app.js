f57e717c7d34bfc297199e2cfc581c0813d0fd09
Bern <bernard.kulegowski@gmail.com>
2020-06-21 14:12:09 +0200

    zad 3 nawet działą

--- lab10/app.js
+++ lab10/app.js
@@ -1,26 +1,47 @@
-const express = require("express");
+// const express = require("express");
+// const app = express();
+
+// var mustacheExpress = require("mustache-express");
+
+// app.engine("mustache", mustacheExpress());
+
+// app.set("view engine", "mustache");
+// app.set("views", __dirname + "/views");
+
+// const calculateTax = (tax, amount) => {
+//     const resoult = (tax * amount) / 100;
+//     return { tax: resoult, amount: amount - resoult };
+// };
+
+// app.get("/podatek/:tax/:amount", function (req, res) {
+//     const { tax, amount } = req.params;
+
+//     res.render("index", {
+//         tax,
+//         amount,
+//         resoult: calculateTax(tax, amount),
+//     });
+// });
+
+const express = require('express');
 const app = express();
 
-var mustacheExpress = require("mustache-express");
-
-app.engine("mustache", mustacheExpress());
-
-app.set("view engine", "mustache");
-app.set("views", __dirname + "/views");
-
-const calculateTax = (tax, amount) => {
-    const resoult = (tax * amount) / 100;
-    return { tax: resoult, amount: amount - resoult };
-};
-
-app.get("/podatek/:tax/:amount", function (req, res) {
-    const { tax, amount } = req.params;
-
-    res.render("index", {
-        tax,
-        amount,
-        resoult: calculateTax(tax, amount),
-    });
+app.get('/:a/:b', (req, res, next) => {
+    try {
+        const { a, b } = req.params;
+        if (Number(b) === 0) {
+            throw new Error('No koniec swiata...');
+        } else {
+            res.send((a / b).toString());
+        }
+    }
+    catch (e) {
+        next(e);
+    }
 });
 
-app.listen(4400, () => console.log("start server"));
+app.use((e, req, res, next) => {
+    res.status(500).send(e.message);
+});
+
+app.listen(4000, () => console.log('start server'));
