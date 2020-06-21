4246629dd937b165736b57109a1d04296b93ab10
MaciejKasperowicz <56731981+MaciejKasperowicz@users.noreply.github.com>
2020-06-21 14:20:09 +0200

    3 zadanie

---
+++ lab10/app3.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.get('/:a/:b', (req, res) => {
+    const { a, b } = req.params;
+    console.log(a)
+    if (Number(b) === 0) {
+        throw new Error('Dzielisz przez 0!!!!');
+    } else {
+        res.send((a / b).toString());
+    }
+
+});
+
+app.use((error, req, res, next) => {
+    console.log(error.message);
+    // throw error
+    res.send(error.message);
+});
+
+app.listen(4100, () => console.log('start server'));
