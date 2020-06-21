ba03b061b130bd85292d2596a253e49e160a543d
MaciejKasperowicz <56731981+MaciejKasperowicz@users.noreply.github.com>
2020-06-21 13:40:40 +0200

    zad 2

---
+++ lab10/app2.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+var mustacheExpress = require('mustache-express');
+
+app.engine('mustache', mustacheExpress());
+
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+const calculateTax = (tax, amount) => {
+    const resoult = (tax * amount) / 100;
+    return { tax: resoult, amount: amount - resoult };
+};
+
+app.get("/podatek/:tax/:amount", (req, res) => {
+    const { tax, amount } = req.params
+    res.render("index", {
+        tax,
+        amount,
+        resoult: calculateTax(tax, amount)
+    });
+});
+
+app.listen(4400, () => console.log("start server"));
