d78646a86d42cfe50d0f923c775cf0896a06dd45
Bern <bernard.kulegowski@gmail.com>
2020-06-21 13:47:13 +0200

    Zad2

--- lab10/app.js
+++ lab10/app.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const app = express();
 
-app.set("view engine", "pug");
-// app.get("/:name", function (req, res) {
-//   const scope = { title: "some title", header: "heloo!" };
-//   res.render("index", scope);
-// });
+var mustacheExpress = require("mustache-express");
 
-app.get("/:name?", function (req, res) {
-    const { name = "World" } = req.params;
+app.engine("mustache", mustacheExpress());
+
+app.set("view engine", "mustache");
+app.set("views", __dirname + "/views");
+
+const calculateTax = (tax, amount) => {
+    const resoult = (tax * amount) / 100;
+    return { tax: resoult, amount: amount - resoult };
+};
+
+app.get("/podatek/:tax/:amount", function (req, res) {
+    const { tax, amount } = req.params;
+
     res.render("index", {
-        title: "Hello",
-        message: `Hello ${name}!`,
+        tax,
+        amount,
+        resoult: calculateTax(tax, amount),
     });
 });
 
-app.listen(4000, () => console.log("start server"));
+app.listen(4400, () => console.log("start server"));
