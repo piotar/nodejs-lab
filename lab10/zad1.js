7d6e277f6287d1634ed069a8e565882444633bc6
Krzysiek <krzysztof.marciniak.123@gmail.com>
2020-06-21 13:11:39 +0200

    zad1

---
+++ lab10/zad1.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+
+app.set("view engine", "pug");
+
+
+app.get('/:name?', function (req, res) {
+    const {name= "World"} = { req.params };
+    res.render("index", {
+        title: "Hello",
+        message: `Hello ${name}!`,
+  });
+});
+
+app.listen(4400, () => console.log("start server"));
