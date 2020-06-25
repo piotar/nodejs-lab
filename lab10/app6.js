8c8eafd8e52d65de2729050e3d8d3215840610b8
Daniel Majewski <danielmajewski82@gmail.com>
2020-06-25 13:25:21 +0200

    task 6

---
+++ lab10/app6.js
@@ -0,0 +1,66 @@
+const mustacheExpress = require("mustache-express");
+const axios = require("axios").default;
+const express = require("express");
+const app = express();
+
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", __dirname + "/views");
+
+const getUser = async (id) => {
+  const url = `https://jsonplaceholder.typicode.com/users/${id}`;
+  const user = await axios.get(url);
+  return user.data;
+};
+
+const getWeather = async (userInfo) => {
+  const lat = userInfo.address.geo.lat;
+  const lng = userInfo.address.geo.lng;
+  const url = `https://api.openweathermap.org/data/2.5/weather?appid=0ed761300a2725ca778c07831ae64d6e&lat=${lat}&lon=${lng}`;
+  const weather = await axios.get(url);
+  return weather.data;
+};
+
+app.get("/user/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const userInfo = await getUser(id);
+    const weatherInfo = await getWeather(userInfo);
+    // res.send({
+    //   name: userInfo.name,
+    //   username: userInfo.username,
+    //   email: userInfo.email,
+    //   address: userInfo.address,
+    //   weather: weatherInfo.weather,
+    //   temp: weatherInfo.main.temp,
+    // });
+    res.render("user6", {
+      name: userInfo.name,
+      username: userInfo.username,
+      email: userInfo.email,
+      city: userInfo.address.city,
+      weather: weatherInfo.weather[0].description,
+      temp: weatherInfo.main.temp,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((error, req, res, next) => {
+  res.render("app6", {
+    error: error.message,
+  });
+});
+
+// app.use((e, req, res, next) => {
+//   if (e.code === "ENOENT") {
+//     console.log(e);
+//     res.render("error");
+//   } else {
+//     console.log(e);
+//     res.status(500).send(e.message);
+//   }
+// });
+
+app.listen(4000, () => console.log("start server"));
