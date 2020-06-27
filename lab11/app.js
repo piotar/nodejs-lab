bbb76ca5b1e303928336c094d6d42371077fc171
Agnieszka Jarosik <jarosikloj@gmail.com>
2020-06-27 16:10:58 +0200

    zadanie2

--- lab11/app.js
+++ lab11/app.js
@@ -1,5 +1,30 @@
+const { ObjectId } = require('mongodb');
+
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
+
+function createTask (task, completed) {
+  return { task, completed }
+}
+
+async function create(collection, task, completed) {
+  try {
+    const taskItem = createTask(task, completed);
+    await collection.insertOne(taskItem);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function getTodos(collection){
+  const result = await collection.find().toArray();
+  console.log(result);
+  return result;
+}
+
+async function updateCompleted(collection, id, completed) {
+
+}
 
 const uri = process.env.MONGODB_CONNECTION;
 const client = new MongoClient(uri, {
@@ -9,8 +34,18 @@ const client = new MongoClient(uri, {
 client.connect(async err => {
   const collection = client.db('todo').collection('tasks');
 
-  const result = await collection.find().toArray();
-  console.log(result);
+  await getTodos(collection);
+
+  // await create(collection, 'kupic jajka', false);
+  // await getTodos(collection);
+
+  // await collection.updateOne({ _id: ObjectId('5ef74ef4385aa524f0dbea8b') }, {$set: {completed: true }});
+  // await getTodos(collection);
+
+  const deleteResutl = await collection.deleteOne({ _id: ObjectId('5ef74ef4385aa524f0dbea8b') } );
+  // console.log(deleteResutl);
+  await getTodos(collection);
+
   client.close();
 })
 
