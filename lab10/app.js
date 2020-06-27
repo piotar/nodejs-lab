1902113f70364294135c514422340bf89057ad60
Katarzyna Górska <katarzyna.redlinska@gmail.com>
2020-06-27 11:20:00 +0200

    lab10 zadanie 2

--- lab10/app.js
+++ lab10/app.js
@@ -5,25 +5,52 @@
 // '/Adam' - wyświetli przywitanie 'hello Adam!' jako nagłówek ('h1')
 // ...
 
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-app.set("views", "./views");
-app.set("view engine", "pug");
+// app.set("views", "./views");
+// app.set("view engine", "pug");
 
-app.get("/", (req, res) => {
-  res.render("index", { message: "Hello World!" });
-});
+// app.get("/", (req, res) => {
+//   res.render("index", { message: "Hello World!" });
+// });
 
-app.get("/:name?", (req, res) => {
-  const { name = "world" } = req.params;
-  res.render("index", { message: `Hello ${name}!` });
-});
+// app.get("/:name?", (req, res) => {
+//   const { name = "world" } = req.params;
+//   res.render("index", { message: `Hello ${name}!` });
+// });
 
-app.listen(4700, () => console.log("Server listen at port: 4700"));
+// app.listen(4700, () => console.log("Server listen at port: 4700"));
 
 // Zadanie 2
 // Przećwiczmy wczytywanie szablonów zmieniając system szablonów PUG na MUSTACHE
 // (https://github.com/bryanburgers/node-mustache-express). Stwórzmy aplikację, która wyliczy nam podatek z
 // podanej kwoty i zwróci nam widok z danymi.
 // Przykład ścieżki /podatek/19/25, gdzie 19 to wysokość vat podana w procentach oraz 25 kwota.
+
+const express = require("express");
+const app = express();
+
+var mustacheExpress = require("mustache-express");
+app.engine("mustache", mustacheExpress());
+
+app.set("view engine", "mustache");
+app.set("views", __dirname + "/views");
+
+const calculateTax = (tax, amount) => {
+  const resultTax = (tax * amount) / 100;
+  return { tax: resultTax, amount: amount - resultTax };
+};
+
+app.get("/podatek/:tax/:amount/", function (req, res) {
+  const result = calculateTax(req.params.tax, req.params.amount);
+  const cena = req.params.amount;
+  const podatek = req.params.tax;
+  const obliczony_podatek = result.tax;
+  const kwota_bez_podatku = result.amount;
+
+  const scope = { cena, podatek, obliczony_podatek, kwota_bez_podatku };
+  res.render("index", scope);
+});
+
+app.listen(4700, () => console.log("Server listen at port: 4700"));
