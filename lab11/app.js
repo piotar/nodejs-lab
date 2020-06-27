bca073ceb5270df567e288eb9333f1e74df41dcd
Piotr Tarasiuk <piotr.tarasiuk@softwarehut.com>
2020-06-27 15:53:40 +0200

    add new task

--- lab11/app.js
+++ lab11/app.js
@@ -3,14 +3,41 @@ require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 
 
+
+
+function createTask(task, completed) {
+    return {task, completed}
+}
+
+async function create(collection, task, completed) {
+    try {
+        const taskItem = createTask(task, completed);
+        await collection.insertOne(taskItem);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function getTodos(collection) {
+    const result = await collection.find().toArray();
+    console.log(result);
+    return result;
+}
+
+
 const uri = process.env.MONGODB_CONNECTION;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(async err => {
     const collection = client.db("todos").collection("tasks");
 
-    const result = await collection.find().toArray();
-    console.log(result);
+    await getTodos(collection)
+
+    await create(collection, 'kupic jajka', false);
+
+    
+    await getTodos(collection)
+
 
     // perform actions on the collection object
     client.close();
