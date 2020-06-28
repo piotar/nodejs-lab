c1980d4cd46b2ad7c4658182b696bbe7d4e3d597
Piotr Tarasiuk <piotr.tarasiuk@softwarehut.com>
2020-06-28 17:23:18 +0200

    Add and update tasks

--- lab11/app.js
+++ lab11/app.js
@@ -1,15 +1,32 @@
 require('dotenv').config();
 
-
+const { ObjectId } = require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
 const uri = process.env.MONGODB_CONNECTION;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(async err => {
     const collection = client.db("todo").collection("tasks");
+    // const usersCollection = client.db("todo").collection("users");
 
-    const result = await collection.find().toArray();
-    console.log(result);
+    // console.log(await collection.find().toArray());
 
+    // await collection.insertOne({
+    //     task: "kupić lody",
+    //     completed: false,
+    //     createDate: '28.06.2020'
+    // });
+
+    // console.log(await collection.find({ task: 'kupić mleko' }).toArray());
+
+
+    // await collection.updateMany({}, { $set : { createDate: '28.06.2020' }})
+
+
+    await collection.updateOne({ _id: ObjectId("5ef8b39a4691151dc8df51ce") }, { $set: { completed: true }})
+
+
+
+    console.log(await collection.find().toArray());
 
     client.close();
 });
