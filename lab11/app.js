ee1ace7ba8828476a1a0152a26bc3fe7991911aa
macstojak <maciej.stojak89@wp.pl>
2020-06-28 18:06:26 +0200

    zad4

--- lab11/app.js
+++ lab11/app.js
@@ -3,9 +3,19 @@ require('dotenv').config();
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true  });
 
+
+const User = mongoose.model('users', {
+    firstName: String,
+    lastName: String,    
+})
+
 const Task = mongoose.model('tasks', {
     task: String,
     completed: Boolean,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+    }
 });
 
 (async ()=>{
@@ -14,13 +24,19 @@ const Task = mongoose.model('tasks', {
     //     completed: false,
     // });
     // await task.save();
-    const tasks = await Task.find();
-    console.log(tasks)
 
-    const taskMleko = await Task.findById("5ef8a67a9e00895ce14496fa")
-    taskMleko.completed = true;
-    taskMleko.save();
+    // const tasks = await Task.find();
+    // console.log(tasks)
+    // const user = new User({
+    //     firstName: "Jan", 
+    //     lastName: "Nowak"
+    // })
+    // await user.save();
+    // const taskMleko = await (await Task.findById("5ef8a67a9e00895ce14496fa"));
+    // taskMleko.user=user;
+    // taskMleko.completed = true;
+    // await taskMleko.save();
     
-    console.log(await Task.find())
+    console.log(await Task.find({completed: true}).populate("user"))
 
 })()
