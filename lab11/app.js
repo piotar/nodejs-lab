0e514ad125aa2fc50dd3c26c4510fb187023aac9
macstojak <maciej.stojak89@wp.pl>
2020-06-28 17:50:33 +0200

    zad3

--- lab11/app.js
+++ lab11/app.js
@@ -1,27 +1,26 @@
 require('dotenv').config();
 
-const {ObjectId} = require('mongodb');
 const mongoose = require("mongoose");
-const MongoClient = require('mongodb').MongoClient;
-const uri = process.env.MONGODB_CONNECTION;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect( async err => {
-  const collection = client.db("todo").collection("tasks");
-  // perform actions on the collection object
- 
-  
-//   await collection.insertOne({
-//       task: "kupić lody",
-//       completed: false,
-//       createDate: "28.06.2020"
-//   })
-//   console.log(await collection.find({task:"kupić mleko"  }).toArray());
-// await collection.updateMany({}, { $set:{createDate:"28.06.2020"}})
+mongoose.connect(process.env.MONGODB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true  });
 
-// 
-
-const result = await collection.deleteMany({_id:ObjectId("5ef8b39bf0a546145008ea82")})
-console.log(result)
-console.log(await collection.find().toArray());
-  client.close();
+const Task = mongoose.model('tasks', {
+    task: String,
+    completed: Boolean,
 });
+
+(async ()=>{
+    // const task = new Task({
+    //     task: "kupić czekoladę",
+    //     completed: false,
+    // });
+    // await task.save();
+    const tasks = await Task.find();
+    console.log(tasks)
+
+    const taskMleko = await Task.findById("5ef8a67a9e00895ce14496fa")
+    taskMleko.completed = true;
+    taskMleko.save();
+    
+    console.log(await Task.find())
+
+})()
