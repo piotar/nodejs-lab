a4a162c8b9e43cbd22a62d3c93c6c23f494d85ae
Radek Kuczynski <krooopel@gmail.com>
2020-07-04 08:34:22 +0200

    started task zad2

--- lab11/app.js
+++ lab11/app.js
@@ -1,6 +1,24 @@
 require("dotenv").config();
 
 const MongoClient = require("mongodb").MongoClient;
+
+const createTask = (task, complited) => ({ task, complited });
+
+async function creat(collection, task, complited) {
+  try {
+    const taskItem = createTask(task, complited);
+    await collection.insertOne(taskItem);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getTodos(collection) {
+  const result = await collection.find().toArray();
+  console.log(result, "\n---------------------------");
+  return result;
+}
+
 const uri = process.env.MONGODB_CONNETION;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -8,7 +26,12 @@ const client = new MongoClient(uri, {
 });
 client.connect(async (err) => {
   const collection = client.db("todo").collection("tasks");
-  const result = await collection.find().toArray();
-  console.log(result);
+
+  getTodos(collection);
+
+  await creat(collection, "Pomalować płot", false);
+
+  getTodos(collection);
+
   client.close();
 });
