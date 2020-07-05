2a79be44661fbb1973ec92115ad027fcd7a2dcf7
Radosław Kuczyński <krooopel@gmail.com>
2020-07-05 15:55:31 +0200

    zad2

--- lab11/app.js
+++ lab11/app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const { ObjectId } = require("mongodb");
 const MongoClient = require("mongodb").MongoClient;
 
 const createTask = (task, complited) => ({ task, complited });
@@ -27,9 +28,21 @@ const client = new MongoClient(uri, {
 client.connect(async (err) => {
   const collection = client.db("todo").collection("tasks");
 
+  // await getTodos(collection);
+
+  // await creat(collection, "Pomalować płot", false);
+
   await getTodos(collection);
 
-  await creat(collection, "Pomalować płot", false);
+  // await collection.updateMany(
+  //   { _id: ObjectId("5f000d6d83254032d65f4d2c") },
+  //   { $set: { complited: true } }
+  // );
+
+  const deleteResult = await collection.deleteOne({
+    _id: ObjectId("5f01d20051525f15174374ab"),
+  });
+  console.log(deleteResult);
 
   await getTodos(collection);
 
